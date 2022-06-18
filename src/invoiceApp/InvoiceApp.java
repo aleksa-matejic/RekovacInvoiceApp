4fc872814b1e88f582576667de35c42b1a0047c5
Aleksa <aljosa.gk@gmail.com>
2022-06-18 21:13:36 +0200

    Added trial period for testing

--- src/invoiceApp/InvoiceApp.java
+++ src/invoiceApp/InvoiceApp.java
@@ -4,13 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import java.time.LocalDateTime;
 
 public class InvoiceApp extends Application
 {
     @Override
     public void start(Stage stage) throws Exception
     {
+        LocalDateTime dateNow = LocalDateTime.now();
+        LocalDateTime dateExpires = LocalDateTime.of(2022, 6, 24, 0, 0);
+
+        boolean trialExpired = dateExpires.isBefore(dateNow);
+
+        if (trialExpired) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Probna verzija istekla!");
+            alert.setHeaderText("Probna verzija aplikacije je istekla, molimo da se javite developeru na mail aljosa.gk@gmail.com");
+            alert.setContentText("Molimo pritisnite OK da ugasite aplikaciju.");
+            alert.showAndWait();
+            System.exit(1);
+        }
+
         Parent root = (Parent) FXMLLoader.load(getClass().getResource("invoice.fxml"));
 
         Scene scene = new Scene(root);

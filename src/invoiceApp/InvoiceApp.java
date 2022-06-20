5dfd7db1c83fbe53a6528ecd9856924d01686324
Aleksa <aljosa.gk@gmail.com>
2022-06-20 04:28:36 +0200

    Added default error handler with alert

--- src/invoiceApp/InvoiceApp.java
+++ src/invoiceApp/InvoiceApp.java
@@ -1,12 +1,15 @@
 package invoiceApp;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class InvoiceApp extends Application
@@ -14,6 +17,8 @@ public class InvoiceApp extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
+        Thread.setDefaultUncaughtExceptionHandler(InvoiceApp::showError);
+
         LocalDateTime dateNow = LocalDateTime.now();
         LocalDateTime dateExpires = LocalDateTime.of(2022, 6, 24, 0, 0);
 
@@ -41,5 +46,23 @@ public class InvoiceApp extends Application
     public static void main(String[] args)
     {
         launch(args);
+    }
+
+    private static void showError(Thread t, Throwable e) {
+        System.err.println("***Default exception handler***");
+        if (Platform.isFxApplicationThread()) {
+            showErrorDialog(e);
+        } else {
+            e.printStackTrace();
+            System.err.println("An unexpected error occurred in " + t);
+        }
+    }
+
+    private static void showErrorDialog(Throwable e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Greska!");
+        alert.setHeaderText("Greska u kodu '" + e.toString() + "', molimo da se javite developeru na mail aljosa.gk@gmail.com");
+        alert.setContentText("Molimo pritisnite OK.");
+        alert.showAndWait();
     }
 }

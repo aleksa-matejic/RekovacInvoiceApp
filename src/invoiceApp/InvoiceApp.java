7c9eeaf4262839d37b818e6f483780e778affb81
Aleksa <aljosa.gk@gmail.com>
2022-06-29 03:27:38 +0200

    Removed trial version

--- src/invoiceApp/InvoiceApp.java
+++ src/invoiceApp/InvoiceApp.java
@@ -19,21 +19,6 @@ public class InvoiceApp extends Application
     {
         Thread.setDefaultUncaughtExceptionHandler(InvoiceApp::showError);
 
-        LocalDateTime dateNow = LocalDateTime.now();
-        LocalDateTime dateExpires = LocalDateTime.of(2022, 6, 24, 0, 0);
-
-        boolean trialExpired = dateExpires.isBefore(dateNow);
-
-        if (trialExpired)
-        {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Probna verzija istekla!");
-            alert.setHeaderText("Probna verzija aplikacije je istekla, molimo da se javite developeru na mail aljosa.gk@gmail.com");
-            alert.setContentText("Molimo pritisnite OK.");
-            alert.showAndWait();
-            System.exit(1);
-        }
-
         Parent root = (Parent) FXMLLoader.load(getClass().getResource("invoice.fxml"));
 
         Scene scene = new Scene(root);
